66a973c6b9e16b58cc557a5af35c86befcb8dae9
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2022-03-27 23:52:24 +0700

    change local state to redux

--- bookapp_redux/src/Pages/Register/index.js
+++ bookapp_redux/src/Pages/Register/index.js
@@ -1,48 +1,50 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import {Button, Input} from '../../Components';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { setForm } from '../../redux';
 
 const Register = () => {
-  const RegisterReducer = useSelector(state => state.RegisterReducer);
-  const [form, setForm] = useState({
-    name: '',
-    email: '', 
-    password: '',
-  });
+  const {form} = useSelector(state => state.RegisterReducer);
+  const dispatch = useDispatch();
+  // const [form, setForm] = useState({
+  //   name: '',
+  //   email: '', 
+  //   password: '',
+  // });
 
   const sendData = () => {
-    console.log('data yang dikirim: ', form);
+    console.log('form: ', form);
+    
   };
 
   // Apabila input sedang berubah maka terima value dari form
-  const onInputChange = (value, input) => {
+  const onInputChange = (value, inputType) => {
     // Rubah form yang lama
-    setForm({
-      // Ambil form yang lama, Jika tidak maka properti sebelumnya akan di remove
-      ...form,
-      // dengan value yang baru
-      [input]: value,
-    });
+    // setForm({
+    //   // Ambil form yang lama, Jika tidak maka properti sebelumnya akan di remove
+    //   ...form,
+    //   // dengan value yang baru
+    //   [input]: value,
+    // });
+    dispatch(setForm(inputType, value))
   };
-
-  useEffect(() => {
-    console.log('global: ', RegisterReducer);
-  }, [])
 
   return (
     <View>
-      <Text>{RegisterReducer.title}</Text>
+      <Text>{form.name}</Text>
       <Input
         placeholder="Full Name"
-        value={form.fullName}
+        value={form.name}
         onChangeText={value => onInputChange(value, 'name')}
       />
+      <Text>{form.email}</Text>
       <Input
         placeholder="Email"
         value={form.email}
         onChangeText={value => onInputChange(value, 'email')}
       />
+      <Text>{form.password}</Text>
       <Input
         placeholder="Password"
         value={form.password}
--- bookapp_redux/src/redux/reducer.js
+++ bookapp_redux/src/redux/reducer.js
@@ -4,6 +4,11 @@ const initialState = {
   name: 'Bayu Cucan',
 }
 const initialStateRegister = {
+  form: {
+    name: '',
+    email: '', 
+    password: '',
+  },
   title: 'Register',
 }
 const initialStateLogin = {
@@ -12,6 +17,21 @@ const initialStateLogin = {
 }
 
 const RegisterReducer = (state = initialStateRegister, action) => {
+  if (action.type === 'SET_TITLE') {
+    return  {
+      ...state,
+      title: 'Register ganti title'
+    }
+  }
+  if (action.type === 'SET_FORM') {
+    return {
+      ...state,
+      form: {
+        ...state.form,
+        [action.inputType]: action.inputValue
+      }
+    }
+  }
   return state;
 }
 const LoginReducer = (state = initialStateLogin, action) => {
