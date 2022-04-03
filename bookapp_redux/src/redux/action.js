9b0d75f7605f03817eddb3bbdb5aa5b7398c14c7
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2022-04-03 15:22:23 +0700

    Create the register feature

--- bookapp_redux/src/redux/action.js
+++ bookapp_redux/src/redux/action.js
@@ -1,3 +1,6 @@
+import axios from 'axios'
+import {Alert} from 'react-native'
+
 export const setForm = (inputType, value) => {
   return {
     type: 'SET_FORM',
@@ -5,3 +8,25 @@ export const setForm = (inputType, value) => {
     inputValue: value,
   };
 };
+
+export const register = (data) => ({
+  type: 'REGISTER',
+  payload: data
+})
+
+export const Regis = data => async dispatch => {
+  try {
+    await axios.post('http://code.aldipee.com/api/v1/auth/register', data)
+    .then((response) => {
+      console.log(response.data.message);
+        if (response.data.success) {
+          Alert.alert('Register Success', response.data.message)
+        } else if (response.code === 400){
+          Alert.alert('Register Failed')
+        }
+    })
+    dispatch(register(data))
+  } catch (err) {
+    console.log(err);
+  }
+}
--- bookapp_redux/src/redux/store.js
+++ bookapp_redux/src/redux/store.js
@@ -1,7 +1,19 @@
 import { createStore, applyMiddleware } from "@reduxjs/toolkit";
 import reduxLogger from 'redux-logger';
+import ReduxThunk from 'redux-thunk'
 import reducer from "./reducer";
 
-const store = createStore(reducer, applyMiddleware(reduxLogger));
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { persistStore, persistReducer } from "redux-persist";
+
+persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+}
+
+const configPersist = persistReducer(persistConfig, reducer)
+
+const store = createStore(configPersist, applyMiddleware(ReduxThunk));
+export const persiststor = persistStore(store)
 
 export default store
