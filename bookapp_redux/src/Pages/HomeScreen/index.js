36786347e6517ef6c9090cd07f4e21a4d9464da8
bayucucanh <58686350+bayucucanh@users.noreply.github.com>
2022-04-06 19:41:58 +0700

    Get Api Books

--- bookapp_redux/src/Pages/HomeScreen/index.js
+++ bookapp_redux/src/Pages/HomeScreen/index.js
@@ -1,25 +1,42 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, {useEffect} from 'react'
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
 import {Facebook} from '../../Assets';
 import {Rating, AirbnbRating} from 'react-native-ratings';
+import { useDispatch, useSelector } from 'react-redux';
+import { getDataBooks } from '../../redux';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
+  // const isLoading = useSelector(state => state.appData.isLoading);
+  const user  = useSelector(state => {
+    console.log('State: ', state.appData.user);
+    return state.appData.user;
+  });
+  const books = useSelector(state => {
+    console.log('State: ', state.appData.books);
+    return state.appData.books;
+  })
+  const dispatch = useDispatch();
+  useEffect(() => {
+    console.log(user.tokens.access.token);
+    console.log('book', books);
+    dispatch(getDataBooks(user.tokens.access.token))
+  }, []);
+
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Icon name="rocket" size={30} color="#900" />
-      {/* <Image source={facebook}  style={{ width: 200 }}/> */}
-      <Facebook />
-      <Rating
-        showRating
-        // onFinishRating={this.ratingCompleted}
-        style={{paddingVertical: 10}}
-      />
+      {books.map(book => (
+      <Text>{book.title}</Text>
+      // <Icon name="rocket" size={30} color="#900" />
+      // <Facebook />
+      // <Rating
+      //   showRating
+      //   // onFinishRating={this.ratingCompleted}
+      //   style={{paddingVertical: 10}}
+      // />
+      ))}
     </View>
   )
 }
 
 export default HomeScreen
-
-const styles = StyleSheet.create({})
--- bookapp_redux/src/redux/action.js
+++ bookapp_redux/src/redux/action.js
@@ -1,6 +1,5 @@
-import axios from 'axios'
-import {Alert} from 'react-native'
-import {useNavigation} from '@react-navigation/native'
+import axios from 'axios';
+import {Alert} from 'react-native';
 
 export const setForm = (inputType, value) => {
   return {
@@ -10,44 +9,64 @@ export const setForm = (inputType, value) => {
   };
 };
 
-export const register = (data) => ({
+export const register = data => ({
   type: 'REGISTER',
-  payload: data
-})
+  payload: data,
+});
 
 export const login = (data) => ({
   type: 'LOGIN',
-  payload: data
-})
+  payload: data,
+});
+
+export const getBooks = data => ({
+  type: 'GET_BOOKS',
+  payload: data,
+});
 
 export const Regis = data => async dispatch => {
   try {
-    await axios.post('http://code.aldipee.com/api/v1/auth/register', data)
-    .then((response) => {
-      console.log(response.data.message);
+    await axios
+      .post('http://code.aldipee.com/api/v1/auth/register', data)
+      .then(response => {
+        console.log(response.data.message);
         if (response.data.success) {
-          Alert.alert('Register Success', response.data.message)
-        } 
-    })
-    dispatch(register(data))
+          Alert.alert('Register Success', response.data.message);
+        }
+      });
+    dispatch(register(data));
   } catch (err) {
-    Alert.alert('Register Failed')
+    Alert.alert('Register Failed');
     console.log(err);
   }
-}
+};
 
 export const LoggedIn = data => async dispatch => {
   try {
-    await axios.post('http://code.aldipee.com/api/v1/auth/login', data)
-    .then((response) => {
-      // console.log(response);
-      Alert.alert('Login Success')
-      let userToken = response.data.tokens.access.token
-      console.log(userToken);
-    })
-    dispatch(login(data))
+    await axios
+      .post('http://code.aldipee.com/api/v1/auth/login', data)
+      .then(response => {
+        Alert.alert('Login Success');
+        console.log(response.data);
+        dispatch(login(response.data));
+      });
   } catch (err) {
-    Alert.alert('Register Failed')
+    Alert.alert('Login Failed');
     console.log(err);
   }
-}
+};
+
+export const getDataBooks = token => {
+  return async dispatch => {
+    await axios
+      .get('http://code.aldipee.com/api/v1/books', {
+        headers: {
+          Authorization: 'Bearer ' + token,
+        },
+      })
+      .then(response => {
+        console.log(response);
+        dispatch(getBooks(response.data.results))
+      });
+  };
+};
